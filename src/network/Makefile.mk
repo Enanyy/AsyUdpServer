26963170925a7256a5dc9301b0b084969defa736
Enanyy <742216502@qq.com>
2018-01-26 19:35:16 +0800

    MakeFile

--- src/network/Makefile.mk
+++
@@ -1,6 +0,0 @@
-NETWORKOBJECTS = EndPoint.o\
-				 Packet.o\
-				 EventPoller.o\
-				 EpollPoller.o\
-				 SelectPoller.o\
-				 KqueuePoller.o\
--- src/protocpp/Makefile.mk
+++
@@ -1,2 +0,0 @@
-PROTOCPPOBJECTS = scene.pb.o\
-				  action.pb.o\
